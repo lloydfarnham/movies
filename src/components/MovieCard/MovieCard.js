f8d1b0060053e86e0e72bb504e8545439e1794de
Lloyd Farnham <lloyd1996@gmail.com>
2023-03-19 12:37:24 +0000

    added additional styling and view toggle functionality

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,20 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './MovieCard.css';
 
 function MovieCard(props) {
   const { movie, getGenreNames } = props;
+  const numFilledStars = Math.round(movie.vote_average / 2);
 
   return (
-    <div className="movie-card">
-        <img src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`} alt={movie.title} />
-        <div class="movie-info">
-            <div class="genre-title">
-                <h4>{getGenreNames(movie.genre_ids).join(', ')}</h4>
-                <h2>{movie.title}</h2>
-            </div>
+    <Link className="no-underline" to="">
+        <div className="movie-card">
+            <img src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`} alt={movie.title} />
+            <h4>{getGenreNames(movie.genre_ids).join(', ')}</h4>
+            <h2>{movie.title}</h2>
             <h3>{movie.overview}</h3>
         </div>
-    </div>
+    </Link>
   );
 }
 
--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -6,14 +6,20 @@ import './MovieList.css';
 function MovieList() {
   const [movies, setMovies] = useState([]);
   const [genres, setGenres] = useState([]);
+  const [selectedOption, setSelectedOption] = useState('popular');
 
   useEffect(() => {
-    var apiUrl = 'https://api.themoviedb.org/3/movie/popular?api_key=dc0fb7b28045cd04916b73e857aec4f9&language=en-US';
+    let apiUrl;
+    if (selectedOption === 'popular') {
+      apiUrl = 'https://api.themoviedb.org/3/movie/popular?api_key=dc0fb7b28045cd04916b73e857aec4f9&language=en-US';
+    } else if (selectedOption === 'topRated') {
+      apiUrl = 'https://api.themoviedb.org/3/movie/top_rated?api_key=dc0fb7b28045cd04916b73e857aec4f9&language=en-US';
+    }
 
     axios.get(apiUrl)
       .then(response => setMovies(response.data.results))
       .catch(error => console.log(error));
-  }, []);
+  }, [selectedOption]);
 
   useEffect(() => {
     const genreApi = 'https://api.themoviedb.org/3/genre/movie/list?api_key=dc0fb7b28045cd04916b73e857aec4f9&language=en-US';
@@ -30,10 +36,17 @@ function MovieList() {
     });
   }
 
+  function handleOptionChange(e) {
+    setSelectedOption(e.target.value);
+  }
+
   return (
     <div class="container">
         <div class="title-search">
-            <h1>Popular Movies</h1>
+            <select class="dropdown" id="view" onChange={handleOptionChange}> 
+                <option value="popular">Popular Movies</option>
+                <option value="topRated">Top Rated Movies</option>
+            </select>
             <div class="search">
                 <i class="fa-solid fa-magnifying-glass"></i>
                 <input type="search"></input>
